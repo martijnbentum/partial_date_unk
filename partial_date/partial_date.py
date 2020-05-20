eb63145853eec22ed763d2f3560ad162ef7b52d6
Martijn Bentum <bentummartijn@gmail.com>
2020-05-20 13:03:47 +0200

    accepts alternative human readable format string (.pretty_string()) for partial date creation handles different month length and leap years for year-month partial date (ends on the last day of the month) defined __eq__ equality method (needed for django) prints human readable dates for the year; year-month; year-month-day formats

--- partial_date/partial_date.py
+++ partial_date/partial_date.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from functools import reduce
 
 '''
 -goal: a custom Django field that accepts input with different levels of date specificity, ranging from a specific day to a millennium
@@ -21,6 +22,15 @@ century     20c           <integer>c     1900-2000
 millenium   2m            <integer>m     1000-2000
 
 For december fourth 1999 you type: 1999-12-04
+
+Alternative formats
+
+(format) 			(Alternative format)
+2c 					2nd century
+4m 					4th millenium
+1999y 				1999
+1999-01 			January 1999
+1999-01-31 			January 31, 1999
 '''
 
 class PartialDate:
@@ -33,14 +43,15 @@ class PartialDate:
 		
 		self.s = s
 		self.format_help = format_help
-		self.format_error = ValueError('input string does not conform to format ' 
-			+ str(self.s) + '\n' + self.format_help)
+		self.format_error = ValueError('input string does not conform to format "' 
+			+ str(self.s) + '"\n' + self.format_help)
 		self.type_dict = {'y':'year','d':'decade','c':'century','m':'millenium',
 			'ym':'year_month','ymd':'year_month_day'}
 		self.type2number_dict = {'year':0,'decade':1,'century':2,'millenium':3,
 			'year_month':4,'year_month_day':5}
 		self.number2type_dict = reverse_dict(self.type2number_dict)
 		self.type2multiplier = {'decade':10,'century':100,'millenium':1000}
+		self.month_dict = dict([[datetime.datetime(2020, i,1).strftime('%B'),i] for i in range(1,13)])
 		if s == None and t == None: raise ValueError('please provide string or datetime object')
 		if s:
 			self.determine_type()
@@ -69,24 +80,43 @@ class PartialDate:
 		elif type(other) == datetime.datetime: start_dt, end_dt = dt, dt
 		else: raise ValueError('cannot compare with ' + str(type(other)) + ' ' + other)
 		return self.start_dt <= start_dt and self.end_dt >= end_dt
+
+	def __eq__(self,other):
+		if type(self) == type(other): 
+			return self.dt == other.dt
+		return False
 		
 
 	def determine_type(self):
 		'''based on the string format the level of date specificity is determined.'''
 		self.year, self.month, self.day = 1,1,1
 		if self.s == '': raise self.format_error
-		if self.s[-1] in self.type_dict.keys():
+		elif check_month(self.s):
+			self.month = self.month_dict[check_month(self.s)]
+			self.year = int(self.s.split(' ')[-1])
+			if self.s.count(' ') == 2:
+				self.day = int(self.s.split(' ')[1].strip(','))
+				self.type = 'year_month_day'
+			else: self.type = 'year_month'
+		elif check_year(self.s):
+			self.type = 'year'
+			self.number = check_year(self.s)
+			self.year = self.number
+		elif check_decade_century_milenium(self.s):
+			self.type = check_decade_century_milenium(self.s)
+			self.number = extract_number_from_count_string(self.s)
+		elif self.s[-1] in self.type_dict.keys():
 			try: setattr(self,self.type_dict[self.s[-1]],int(self.s[:-1]))
 			except: raise self.format_error
 			else: 
 				self.type = self.type_dict[self.s[-1]]
 				self.number = int(self.s[:-1])
 		elif self.s.count('-') == 1:
-			try: self.year, self.month = reduce(int,self.s.split('-'))
+			try: self.year, self.month = [int(s) for s in self.s.split('-')]
 			except: raise self.format_error
 			else:self.type = 'year_month'
 		elif self.s.count('-') == 2:
-			try: self.year, self.month,self.day = reduce(int,self.s.split('-'))
+			try: self.year, self.month,self.day = [int(s) for s in self.s.split('-')]
 			except: raise self.format_error
 			else:self.type = 'year_month_day'
 		else: raise self.format_error
@@ -109,7 +139,7 @@ class PartialDate:
 			self.dt = datetime.datetime(year=self.year,month=self.month,day=self.day,microsecond = self.type2number_dict[self.type])
 			self.start_dt = self.dt
 			if self.type == 'year': self.month, self.day =12,31
-			if self.type == 'year_month': self.day = 31
+			if self.type == 'year_month': self.day = month2endday(self.year,self.month)
 			self.end_dt = datetime.datetime(year=self.year,month=self.month,day=self.day,
 				microsecond=self.type2number_dict[self.type])
 
@@ -141,8 +171,8 @@ class PartialDate:
 		if self.type in 'decade,century,millenium'.split(','):
 			return make_count_string(self.number) + ' ' + self.type
 		if self.type == 'year': s='%Y'
-		if self.type == 'year_month': s='%Y-%m'
-		if self.type == 'year_month_day': s='%Y-%m-%d'
+		if self.type == 'year_month': s='%B %Y'
+		if self.type == 'year_month_day': s='%B %d, %Y'
 		return self.dt.strftime(s)
 
 	@property
@@ -178,14 +208,49 @@ class PartialDateField(models.Field):
 	
 def make_count_string(n):
 	'''Create the correct abbreviation for a date (2nd or 4th) for 2nd or 4th century).'''
-	if n == 0 or int(str(n)[-1]) > 3: return str(n)+'th'
+	if n == 0 or int(str(n)[-1]) > 3 or int(str(n)[-1]) == 0: return str(n)+'th'
 	if str(n)[-1] == '1': return str(n)+'st'
 	if str(n)[-1] == '2': return str(n)+'nd'
 	if str(n)[-1] == '3': return str(n)+'rd'
 	raise ValueError('could not parse',n)
-		
-	
+
+def extract_number_from_count_string(s):
+	for abbrev in 'th,st,nd,rd'.split(','):
+		if abbrev in s: return int(s.split(abbrev)[0])
+	raise ValueError('could not extract number from ' +s)
 			
+
+def check_month(s):
+	months = [datetime.datetime(2020, i,1).strftime('%B') for i in range(1,13)]
+	for month in months:
+		if month.lower() in s.lower(): return month
+	return False
+
+def check_year(s):
+	try: return int(s)
+	except: return False
+
+def check_decade_century_milenium(s):
+	for n in 'decade,century,millenium'.split(','):
+		if n in s: return n
+	return False
+
+def month2endday(year, month):
+	# deal with leap years for february 
+	if month == 2:
+		if year % 4 == 0:
+			if year % 100 == 0:
+				if year % 400 == 0: return 29
+				return 28
+			return 29
+		return 28
+	# deal with the short months
+	if month in [4,6,9,11]: return 30
+	# the rest
+	return 31
+			
+			
+
 def reverse_dict(d):
 	'''Swap keys and values does not check whether original values are unique.'''
 	return {v:k for k, v in d.items()}
